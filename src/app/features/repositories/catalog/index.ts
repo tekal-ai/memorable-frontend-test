e05475e4b23b32ad712c7c9d4e37507457701747
Josiah Brown <josiah.webdev@gmail.com>
2023-08-15 18:58:57 -0500

    Add 'Creatives' table in Library page

--- src/app/features/repositories/catalog/index.ts
+++ src/app/features/repositories/catalog/index.ts
@@ -3,6 +3,7 @@ import { UsersBackendRepository } from "./users.repository";
 import { AssetsBackendRepository } from "./assets.repository";
 import { BusinessBackendRepository } from "./business.repository";
 import { EarlyAccessBackendRepository } from "./early-access.repository";
+import { CreativesBackendRepository } from "./creatives.repository";
 
 export const repository = new Map();
 
@@ -11,3 +12,4 @@ repository.set("UsersRepository", UsersBackendRepository);
 repository.set("AssetsRepository", AssetsBackendRepository);
 repository.set("EarlyAccessRepository", EarlyAccessBackendRepository);
 repository.set("BusinessRepository", BusinessBackendRepository);
+repository.set("CreativesRepository", CreativesBackendRepository);
--- src/app/pages/creative-intelligence-suite/pages/creative-lab/pages/creative-library/creative-library.page.tsx
+++ src/app/pages/creative-intelligence-suite/pages/creative-lab/pages/creative-library/creative-library.page.tsx
@@ -1,8 +1,31 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { useSessionFeature } from "src/app/features/session/session.feature";
 import CardPageUI from "src/app/ui/cards/card-page.ui";
 import { SearchInputUI } from "src/app/ui/inputs/search-input.ui";
+import { useCreativesDomain } from "src/domain/creatives/creatives.domain";
+import { CreativeLibraryFolder, CreativeLibraryItem } from "src/graphql/client";
+import { CreativeLibraryTableWidget } from "./creative-library.table.widget";
+// import useSWR from "swr";
 
 const CreativeLibraryPage: FC = () => {
+  const [creativeData, setCreativeData] = useState<CreativeLibraryItem[]>([]);
+  const { currentBrand } = useSessionFeature();
+  const { getCreatives } = useCreativesDomain();
+
+  // const { data: creatives } = useSWR("gettingCreatives", getCreatives);
+
+  useEffect(() => {
+    if (currentBrand?.id?.length) {
+      getCreatives({ brandId: currentBrand?.id })
+        .then((resp: CreativeLibraryFolder | null) => {
+          if (resp?.creatives) setCreativeData(resp?.creatives);
+        })
+        .catch((err) => {
+          throw new Error(`Error getting creatives: ${err}`);
+        });
+    }
+  }, [currentBrand?.id]);
+
   return (
     <CardPageUI>
       <header
@@ -17,7 +40,11 @@ const CreativeLibraryPage: FC = () => {
       >
         <SearchInputUI />
       </header>
-      <pre>Insert Table here</pre>
+      {creativeData.length ? (
+        <CreativeLibraryTableWidget data={creativeData} />
+      ) : (
+        <p>You don't have any brands yet.</p>
+      )}
     </CardPageUI>
   );
 };
